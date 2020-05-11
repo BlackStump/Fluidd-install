d3275c916ee7ea8e38a159e38fd4a30a51c05c6e
BlackStump <altocoey@hotmail.com>
2020-05-11 17:00:16 +1000

    0.0.12

--- pi-pre-install-nginx.sh
+++ pi-pre-install-nginx.sh
@@ -6,7 +6,7 @@ PYTHONDIR="${HOME}/klippy-env"
 SYSTEMDDIR="/etc/systemd/system"
 KLIPPER_USER=$USER
 KLIPPER_GROUP=$KLIPPER_USER
-KWC="https://github.com/meteyou/mainsail/releases/download/V0.0.11/mainsail-alpha-0.0.11.zip"
+KWC="https://github.com/meteyou/mainsail/releases/download/v0.0.12/mainsail-alpha-0.0.12.zip"
 
 # Step 1: Install system packages
 install_packages()
