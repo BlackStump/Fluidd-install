0ad8d97ae5f9937e10c0ec6a4ba8ab89dc4d1819
BlackStump <altocoey@hotmail.com>
2020-09-28 12:36:56 +1000

    Update moonraker-mainsail-install.sh

--- moonraker-mainsail-install.sh
+++ moonraker-mainsail-install.sh
@@ -7,7 +7,7 @@ SYSTEMDDIR="/etc/systemd/system"
 MOONRAKER_USER=$USER
 KLIPPER_USER=$USER
 KLIPPER_GROUP=$KLIPPER_USER
-KWC="https://github.com/BlackStump/mainsail-install/files/5250923/mainsail-beta-0.2.2B.zip"
+KWC="https://github.com/BlackStump/mainsail-install/releases/download/v0.2.2C/mainsail-beta-0.2.2C.zip"
 
 # Step 1: Install system packages
 install_packages()
